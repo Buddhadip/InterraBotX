f10b800721c364053ffecf6406d536175655718a
Buddhadip <mailtobuddhadip@gmail.com>
2023-12-07 18:04:56 +0530

    feat: add routes file for code generation

---
+++ backend/src/routes/codeGeneration.js
@@ -0,0 +1,23 @@
+// codeGeneration.js
+import express from 'express';
+import { generateCode } from '../utils/openai.js'; // Correct import
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const userInput = req.body.userInput;
+
+  try {
+    
+    // Call the OpenAI API for code generation
+    const generatedCode = await generateCode(userInput);
+
+    // Send the generated code as the response
+    res.json({ generatedCode });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+export default router;
