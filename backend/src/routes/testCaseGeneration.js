f68650679044431291988b944596e29162427193
Buddhadip <mailtobuddhadip@gmail.com>
2023-12-07 18:05:16 +0530

    feat: add routes file for test case generation

---
+++ backend/src/routes/testCaseGeneration.js
@@ -0,0 +1,22 @@
+// testCaseGeneration.js
+import express from 'express';
+import { generateTestCases } from '../utils/openai.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const functionSnippet = req.body.functionSnippet;
+
+  try {
+    // Call the OpenAI API for generating test cases with the provided prompt
+    const processedTestCases = await generateTestCases(functionSnippet);
+
+    // Send the processed test cases as the response
+    res.json({ processedTestCases });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+export default router;
